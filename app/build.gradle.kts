8bdbbdedfb49df57d8bbf2e13f09aae23e47e295
Kleer3 <kleerrodriguez0@gmail.com>
2024-11-11 20:56:17 +0100

    Implements retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation ("com.google.code.gson:gson:2.11.0")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
---
+++ app/src/main/java/com/example/superheroes/data/SuperHeroeResponse.kt
@@ -0,0 +1,5 @@
+package com.example.superheroes.data
+
+data class SuperHeroeResponse {
+
+}
