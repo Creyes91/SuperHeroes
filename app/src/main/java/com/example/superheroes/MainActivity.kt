c3268d747404319ff7672cdad47ed2cb7a84e1b3
Kleer3 <kleerrodriguez0@gmail.com>
2024-11-12 18:50:24 +0100

    implements action search

--- app/src/main/java/com/example/superheroes/MainActivity.kt
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.superheroes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
+import com.example.superheroes.utils.RetrofitProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.Objects
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_list)
+
+
+    }
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_menu_main, menu)
+
+        val btnSearch = menu?.findItem(R.id.search_bar)!!
+
+        val searchView = btnSearch.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener
+        {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                searchSuperHeroes(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+
+        })
+
+        return true
+    }
+
+    private fun searchSuperHeroes(query: String?) {
+        val service= RetrofitProvider.GetRetrofit()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = service.findSuperheroesByName(query!!)
+            println(result)
+
+        }
     }
 }
